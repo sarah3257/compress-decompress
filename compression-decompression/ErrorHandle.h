d1ac5d6d769467b4bc3b7d535a03b741b74f48c1
cohentamarwolf <n641935@gmail.com>
2024-07-31 12:39:51 +0300

    style : HandleFile

--- compression-decompression/ErrorHandle.h
+++ compression-decompression/ErrorHandle.h
@@ -8,7 +8,8 @@ public:
 	ErrorHandle(const std::string& logFile);
 	~ErrorHandle();
 	void handleError(const std::string& errorMessage);
-	//static error messages static const std::string CANNOT_OPEN_FILE;
+	//static error messages 
+	static const std::string CANNOT_OPEN_FILE;
 	static const std::string TRYING_TO_DECODE_UNCOMPRESSED_FILE;
 private:
 	std::string logFileName;
