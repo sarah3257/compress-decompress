f4ee02d6a4d84ae951678a80537d796c1204bef1
sarah <sarah134632@gmail.com>
2024-07-31 16:50:49 +0300

    Implemented(LZ77 decomression):  add Implemented function in this class

--- compression-decompression/ErrorHandle.h
+++ compression-decompression/ErrorHandle.h
@@ -7,10 +7,12 @@ class ErrorHandle
 public:
 	ErrorHandle(const std::string& logFile);
 	~ErrorHandle();
-	void handleError(const std::string& errorMessage);
+	static void handleError(const std::string& errorMessage);
 	//static error messages 
     static const std::string CANNOT_OPEN_FILE;
 	static const std::string TRYING_TO_DECODE_UNCOMPRESSED_FILE;
+	static const std::string NO_BUFFER_CHARACTER_FOUND;
+	
 private:
 	std::string logFileName;
 	std::ofstream logFileStream;
