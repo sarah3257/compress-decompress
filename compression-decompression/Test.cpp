df20ea3aa19990606030a8b8c09dcb940535cc9f
ZipiLindenfeld <z0556776310@gmail.com>
2024-08-12 15:28:19 +0300

    get tests

--- compression-decompression/Test.cpp
+++ compression-decompression/Test.cpp
@@ -20,7 +20,7 @@ bool Test::areFilesEqual(const std::string& file1, const std::string& file2) {
     return std::equal(begin1, end1, begin2);
 }
 
-void Test:: createRegularFile(const std::string& filename1) {
+void Test::createRegularFile(const std::string& filename1) {
     const std::string  filename = filename1+".txt";
     const std::string text = "It is with great excitement that we present to you our crazy project.\nA lot of energy and effort was invested in calculating the algorithm\n";
     const size_t fileSize = 10 * 1024;
@@ -228,12 +228,12 @@ void Test::compressAndDecompress(const std::string& filename)
 
 void Test::playTest()
 {
-   /* testRegularFile();
+   testRegularFile();
     testEmptyFile();
     testRandomFile();
     testSmallFile();
     testOneCharacterFile();
     testZeroFile();
-    testControl_Z_File();*/
-    testSizeGBFile();
+    testControl_Z_File();
+    //testSizeGBFile();
 }
