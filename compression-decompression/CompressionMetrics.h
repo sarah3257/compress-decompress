5352fc56ef9f47259fe5cb924ec1208348e700be
cohentamarwolf <n641935@gmail.com>
2024-08-14 13:34:48 +0300

    Implementation: Implementation of lz77 & deflate compression functions performance documentation

--- compression-decompression/CompressionMetrics.h
+++ compression-decompression/CompressionMetrics.h
@@ -14,52 +14,11 @@
 #define MARGIN 50
 class CompressionMetrics
 {
-    static const std::string password;
-
-    double compressionRatio; // ratio
-    double compressionSpeed; // speed
-    double originalSize;     // original size
-    double compressedSize;   // compress size
-
 public:
 
-    CompressionMetrics()
-    {
-
-    }
-
     double HuffmanCompression(const std::string& fileName);
-    void LZ77Compression(const std::string& fileName);
-
-
-    void setMetrics(double originalSize, double compressedSize, double compressionSpeed) {
-        this->originalSize = originalSize;
-        this->compressedSize = compressedSize;
-        this->compressionSpeed = compressionSpeed;
-        if (originalSize > 0) {
-            this->compressionRatio = (originalSize - compressedSize) / originalSize * 100.0;
-        }
-        else {
-            this->compressionRatio = 0.0;
-        }
-    }
-
-    double getCompressionRatio() const {
-        return compressionRatio;
-    }
-
-    double getCompressionSpeed() const {
-        return compressionSpeed;
-    }
-
-    double getOriginalSize() const {
-        return originalSize;
-    }
-
-    double getCompressedSize() const {
-        return compressedSize;
-    }
-
+    double LZ77Compression(const std::string& fileName);
+    double DeflateCompression(const std::string& fileName);
    static void DrawGraph(HDC hdc);
    static LRESULT CALLBACK GraphWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
