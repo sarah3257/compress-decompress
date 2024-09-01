7242ff227eaba1926ff307e995bc7e2e5d446ed8
cohentamarwolf <n641935@gmail.com>
2024-09-01 11:52:02 +0300

    fix (writeBufferCompress, convertToBinaryVector): program improvement

--- compression-decompression/FileStream.cpp
+++ compression-decompression/FileStream.cpp
@@ -2,6 +2,7 @@
 #include "Logger.h"
 #include "StreamHandler.h"
 #include <bitset>
+
 #define M 100
 
 double FileStream::EfficiencyPercentages = 0.0;
@@ -87,14 +88,14 @@ void FileStream::writeMap(const std::unordered_map<char, std::string>& codes) {
 		while (text.size() % 8)
 			text.push_back('0');
 		std::vector<char> buffer;
-		for (int i = 0; i < text.size(); i += 8) {
-			std::string byteString = text.substr(i, 8);
-			std::bitset<8> byte(byteString);
+		for (size_t i = 0; i < text.size(); i += 8) {
+			std::bitset<8> byte(std::string(text.data() + i, 8));
 			buffer.push_back(static_cast<char>(byte.to_ulong()));
 		}
+
 		writeData(strSize);
 		writeData(buffer);
-	}
+	}	
 }
 
 void FileStream::readMap(std::unordered_map<char, std::string>& codes) {
