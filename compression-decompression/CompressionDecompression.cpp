8001cc286d7f03ec8ff239f92794d75531ff2918
sarah <sarah134632@gmail.com>
2024-09-02 10:36:22 +0300

    fix(folder compress)

--- compression-decompression/CompressionDecompression.cpp
+++ compression-decompression/CompressionDecompression.cpp
@@ -7,17 +7,10 @@
 #include <chrono>
 #include <windows.h>
 #include <psapi.h>
+#include <filesystem> 
 
-//הוספה לניהול תיקיות
-#include <filesystem> // כלילת ספרייה לעבודה עם מערכת הקבצים
+namespace fs = std::filesystem; 
 
-namespace fs = std::filesystem; // שימוש בקיצור למרחב השמות של מערכת הקבצים
-
-bool check_path_is_directory(const std::string& fileName) {
-	fs::path path(fileName); // המרת מחרוזת ל- fs::path לצורך עבודה עם מערכת הקבצים
-	return fs::is_directory(path);
-	
-}
 
 const std::string CompressionDecompression::password = "stzip";
 double CompressionDecompression::cpuTime = 0.0;
@@ -63,20 +56,18 @@ void CompressionDecompression:: play(const std::string& fileName, const std::str
 
 //compress the data divided to buffers
 void CompressionDecompression::compress(const std::string& fileName, CompressFunction compressFunc) {
-	//בדיקה אם אתה תקייה או קובץ
-	//אם זה תיקיה 
-	if (check_path_is_directory(fileName)) {
+	fs::path originalPath(fileName);
+	//Checking if you are a folder or a file
+	if (fs::is_directory(originalPath)) {
 
 		fs::path originalPath(fileName);
 		fs::path newPath = originalPath.string() + "STZip";
 		fs::create_directory(newPath);//יצירה
-		// לולאה שעוברת על כל הקבצים בתיקיה
+		// A loop that goes through all the files in the folder
 		for (const auto& entry : fs::directory_iterator(originalPath)) {
-			// בדיקה אם הערך הנוכחי בלולאה הוא קובץ רגיל ולא תיקיה
 			const std::string& fileInFolder=entry.path().string();
-			//מקור-entry.path().string();
-			//יעד
 			const std::string& fileDestination=originalPath.string() + "STZip\\"+entry.path().filename().string();
+
 			if (fs::is_regular_file(entry.status())) {
 				CompressionDecompression::play(fileInFolder, fileDestination, compressFunc);
 			}
