1de2641d474517b12e83765c21b458023e69134b
sarah <sarah134632@gmail.com>
2024-09-03 15:17:23 +0300

    add(google test)

--- Sample-Test2/test.cpp
+++ Sample-Test2/test.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "Huffman.h"
+#include <gtest/gtest.h>
+
 TEST(testsmallinput, TestName) {
 	std::vector<char>vec = { '0','1' };
 	std::unordered_map<char, int> expected = { {'0',1},
@@ -9,16 +11,25 @@ TEST(testsmallinput, TestName) {
   EXPECT_TRUE(true);
 }
 
-//#include <gtest/gtest.h>
-//
-//// דוגמה לבדיקה פשוטה
-//TEST(SimpleTest, Addition) {
-//    EXPECT_EQ(2 + 2, 4);
-//}
-//
-//// פונקציה ראשית להפעלת כל הבדיקות
-//int main(int argc, char **argv) {
-//    ::testing::InitGoogleTest(&argc, argv);
-//    return RUN_ALL_TESTS();
-//}
-//
+
+// דוגמה לבדיקה פשוטה
+TEST(SimpleTest, Addition) {
+    EXPECT_EQ(2 + 2, 4);
+}
+
+// דוגמה לבדיקה עם תנאי
+TEST(SimpleTest, BooleanCondition) {
+    EXPECT_TRUE(3 > 2);
+}
+
+// דוגמה לבדיקה של אוסף
+TEST(SimpleTest, VectorSize) {
+    std::vector<int> vec = { 1, 2, 3, 4, 5 };
+    EXPECT_EQ(vec.size(), 5);
+}
+
+// פונקציה ראשית להפעלת כל הבדיקות
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
