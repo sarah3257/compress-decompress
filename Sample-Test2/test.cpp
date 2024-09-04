c7627979b4216245bd2ada6b9bbe490bfae7fbce
cohentamarwolf <n641935@gmail.com>
2024-09-04 17:40:20 +0300

    fix : solution the warnings of level 4

--- Sample-Test2/test.cpp
+++ Sample-Test2/test.cpp
@@ -4,6 +4,53 @@
 #include "Logger.h"
 #include <gtest/gtest.h>
 
+TEST(LZ77, GetTokensEmptyInput) {
+    std::vector<char> input = {};
+    auto tokens = LZ77::getTokens(input);
+    EXPECT_TRUE(tokens.empty());
+}
+
+TEST(LZ77, GetTokensSingleCharacter) {
+    std::vector<char> input = { 'A' };
+    auto tokens = LZ77::getTokens(input);
+    EXPECT_EQ(tokens.size(), 1);
+    EXPECT_EQ(tokens[0], LZ77Token(0, 0, 'A'));
+}
+
+TEST(LZ77, GetTokensTwoDifferentCharacters) {
+    std::vector<char> input = { 'A', 'B' };
+    auto tokens = LZ77::getTokens(input);
+    EXPECT_EQ(tokens.size(), 2);
+    EXPECT_EQ(tokens[0], LZ77Token(0, 0, 'A'));
+    EXPECT_EQ(tokens[1], LZ77Token(0, 0, 'B'));
+}
+
+TEST(LZ77, GetTokensRepeatedCharacters) {
+    std::vector<char> input = { 'A', 'A', 'A' };
+    auto tokens = LZ77::getTokens(input);
+    EXPECT_EQ(tokens.size(), 2);
+    EXPECT_EQ(tokens[0], LZ77Token(0, 0, 'A'));
+    EXPECT_EQ(tokens[1], LZ77Token(1, 1, 'A'));
+}
+
+TEST(LZ77, GetTokensComplexInput) {
+    std::vector<char> input = { 'A', 'B', 'A', 'A', 'C', 'A', 'B', 'A' };
+    auto tokens = LZ77::getTokens(input);
+
+    std::vector<LZ77Token> expectedTokens = {
+        {0, 0, 'A'},
+        {0, 0, 'B'},
+        {2, 1, 'A'},
+        {0, 0, 'C'},
+        {5, 2, 'A'}
+    };
+
+    EXPECT_EQ(tokens.size(), expectedTokens.size());
+    for (size_t i = 0; i < tokens.size(); ++i) {
+        EXPECT_EQ(tokens[i], expectedTokens[i]);
+    }
+}
+
 TEST(LZ77, Compress) {
 
     // a simple string
@@ -50,6 +97,58 @@ TEST(LZ77, Decompress) {
 
     // compare the outputs
     EXPECT_EQ(decompressedOutput, expectedOutput);
+}
+
+
+TEST(huffmanTest, calculateFrequencies) {
+    std::vector<char>vec = { '0','1' };
+    std::unordered_map<char, int> expected = { {'0',1},
+        {'1',1} };
+    std::unordered_map<char, int> res = Huffman::calculateFrequencies(vec);
+    EXPECT_EQ(expected.size(), res.size());
+    EXPECT_TRUE(true);
+}
+
+//Checks the  compress huffman 
+TEST(HuffmanTest, compress) {
+
+    std::vector<char> text = { 'S','T','Z','I','P' };
+    std::unordered_map<char, std::string> codes;
+    std::unordered_map<char, std::string> codesRes = { {'P',std::string("00")},{'I',std::string("01")},{'T',std::string("10")},{'S',std::string("110")},{'Z',std::string("111")} };
+
+    std::vector<char> vectorExpected = Huffman::compress(text, codes);
+    std::vector<char> vectorResult = { -41,'@',4 };
+
+    //Checks the text result
+    EXPECT_EQ(vectorExpected.size(), vectorResult.size());
+    for (int i = 0; i < vectorExpected.size(); ++i) {
+        EXPECT_EQ(vectorExpected[i], vectorResult[i]);
+    }
+    //Checks the codes result
+    for (auto item : codes) {
+        std::string s = codesRes[item.first];
+        std::string s2 = item.second;
+        EXPECT_EQ(codesRes[item.first], item.second);
+    }
+
+}
+
+//Checks the  decompress huffman 
+TEST(HuffmanTest, decompress) {
+
+    std::vector<char> text = { '1','1','0','1','0','1','1','1','0','1','0','0' };
+    std::unordered_map<char, std::string> codes = { {'P',std::string("00")},{'I',std::string("01")},{'T',std::string("10")},{'S',std::string("110")},{'Z',std::string("111")} };
+
+    std::vector<char> vectorExpected = Huffman::decompress(text, codes);
+    std::vector<char> vectorResult = { 'S','T','Z','I','P' };
+
+    //Checks the text result
+    EXPECT_EQ(vectorExpected.size(), vectorResult.size());
+    for (int i = 0; i < vectorExpected.size(); ++i) {
+        EXPECT_EQ(vectorExpected[i], vectorResult[i]);
+    }
+
+
 }
 
 TEST(testsmallinput, TestName) {
